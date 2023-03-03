5afee047b7da6d721f8263c44e0ff4f9f0ea40f5
Aniket2421 <115690076+Aniket2421@users.noreply.github.com>
2023-03-04 00:53:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your code here
+$(function() {
+	$( "#parent" ).sortable({
+      revert: true
+    });
+  $(".draggable").draggable({
+	  // connectToSortable: "#sortable",
+   //  revert: "invalid",
+   //  zIndex: 100
+	 connectToSortable: "#sortable",
+      helper: "clone",
+      revert: "invalid"
+  });
+
+  $(".droppable").droppable({
+    drop: function(event, ui) {
+      var draggableId = ui.draggable.attr("id");
+      var droppableId = $(this).attr("id");
+      var draggableBg = ui.draggable.css("background-image");
+      var droppableBg = $(this).css("background-image");
+      
+      ui.draggable.css("background-image", droppableBg);
+      $(this).css("background-image", draggableBg);
+    }
+  });
+});
